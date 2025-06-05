2650f98afc26aeab1c7b1b856a2c19e1e4206991
Henry Rose <henryrose@gmail.com>
2025-06-05 09:03:05 -0700

    Initial commit: React + Leaflet map centered on Nassau

---
+++ src/components/Map.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+// Fix for default marker icons in Leaflet with React
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+});
+
+const NASSAU_COORDINATES: [number, number] = [25.0343, -77.3963];
+
+const Map: React.FC = () => {
+  return (
+    <MapContainer
+      center={NASSAU_COORDINATES}
+      zoom={13}
+      style={{ height: '100vh', width: '100%' }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={NASSAU_COORDINATES}>
+        <Popup>
+          Nassau, Bahamas
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default Map; 
